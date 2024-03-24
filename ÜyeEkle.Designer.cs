cdc6f530d198a0a05bf9a3e89ff57927abdf2c3e
habibekrgz129@gmail.com <habibekrgz129@gmail.com>
2024-03-24 13:59:14 +0300

    Görev1Proje dosyası ekle.

--- ÜyeEkle.Designer.cs
+++ ÜyeEkle.Designer.cs
@@ -28,88 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox3 = new TextBox();
-            textBox2 = new TextBox();
-            textBox1 = new TextBox();
-            label3 = new Label();
-            label2 = new Label();
-            label1 = new Label();
             button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
             SuspendLayout();
             // 
-            // textBox3
+            // button1
             // 
-            textBox3.Location = new Point(177, 61);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(154, 23);
-            textBox3.TabIndex = 13;
-            textBox3.TextChanged += this.textBox3_TextChanged;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(177, 23);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(154, 23);
-            textBox2.TabIndex = 12;
-            textBox2.TextChanged += this.textBox2_TextChanged;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(177, 99);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(154, 23);
-            textBox1.TabIndex = 11;
-            textBox1.TextChanged += this.textBox1_TextChanged;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(12, 107);
-            label3.Name = "label3";
-            label3.Size = new Size(93, 15);
-            label3.TabIndex = 10;
-            label3.Text = "Mailinizi Giriniz: ";
-            label3.Click += this.label3_Click;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(12, 69);
-            label2.Name = "label2";
-            label2.Size = new Size(102, 15);
-            label2.TabIndex = 9;
-            label2.Text = "Soyadınızı Giriniz: ";
-            label2.Click += this.label2_Click;
+            button1.Location = new Point(73, 145);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Kaydet";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 31);
+            label1.Location = new Point(21, 40);
             label1.Name = "label1";
             label1.Size = new Size(85, 15);
-            label1.TabIndex = 8;
+            label1.TabIndex = 1;
             label1.Text = "Adınızı Giriniz: ";
-            label1.Click += this.label1_Click;
             // 
-            // button1
+            // label2
             // 
-            button1.Location = new Point(114, 150);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 7;
-            button1.Text = "Ekle";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            label2.AutoSize = true;
+            label2.Location = new Point(21, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Soyadınızı Giriniz: ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(152, 84);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(152, 40);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 4;
             // 
             // ÜyeEkle
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
-            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(button1);
@@ -121,12 +95,10 @@
 
         #endregion
 
-        private TextBox textBox3;
-        private TextBox textBox2;
-        private TextBox textBox1;
-        private Label label3;
-        private Label label2;
-        private Label label1;
         private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
--- ÜyeEkle.cs
+++ ÜyeEkle.cs
@@ -1,4 +1,11 @@
-﻿using gorselProgramlama;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using gorselProgramlama;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,19 +20,19 @@ namespace GörselProgramlamaGörev1
 {
     public partial class ÜyeEkle : Form
     {
-        
+
         private List<Uyeler> uyeler;
 
         public ÜyeEkle()
         {
             InitializeComponent();
             uyeler = new List<Uyeler>();
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Uyeler uye=new Uyeler();
+            Uyeler uye = new Uyeler();
             uye.UyeAdi = textBox1.Text;
             uye.UyeSoyadi = textBox2.Text;
 
